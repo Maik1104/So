a0f9213d4cf546ce764a8496b703d98b9b7a2453
Michael Zapata <mizapataa@unal.edu.co>
2020-12-05 17:56:31 -0500

    a

--- proyecto/proyecto/views.py
+++ proyecto/proyecto/views.py
@@ -5,5 +5,5 @@ from subprocess import *
 def inicio(request):
 
     ubicacion=getoutput("pwd")
-    carpetas=getoutput("find . -maxdepth 1 -type f ")
+    carpetas=getoutput("find . -maxdepth 1 -type d")
     return render(request, "index.html", {"ubicacion":ubicacion, "carpetas":carpetas})
