518e2325be442d2e8c89819145ae4d75e612eac7
Michael Zapata <mizapataa@unal.edu.co>
2020-12-05 18:10:33 -0500

    a

--- proyecto/proyecto/views.py
+++ proyecto/proyecto/views.py
@@ -4,9 +4,10 @@ from subprocess import *
 
 def inicio(request):
 
-    ubicacion=getoutput("pwd")
-    carpetas=getoutput("find . -maxdepth 1 -type d")
-    carpetas = carpetas.split(" ")
-    for i in range(len(carpetas)):
-        carpetas[i] = carpetas[i][2:]
+    ubicacion = getoutput("pwd")
+    carpetas = getoutput("find . -maxdepth 1 -type d")
+    carpetas = carpetas.split("\n")
+    carpetas2 = []
+    for i in range(1, len(carpetas)):
+        carpetas2.append(carpetas[i][2:])
     return render(request, "index.html", {"ubicacion":ubicacion, "carpetas":carpetas})
