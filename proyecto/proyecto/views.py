91cd7c757806b47c7e62a73e0bcbab2dc2603094
Michael Zapata <mizapataa@unal.edu.co>
2020-12-05 18:03:45 -0500

    a

--- proyecto/proyecto/views.py
+++ proyecto/proyecto/views.py
@@ -7,4 +7,6 @@ def inicio(request):
     ubicacion=getoutput("pwd")
     carpetas=getoutput("find . -maxdepth 1 -type d")
     carpetas = carpetas.split(" ")
+    for i in range(len(carpetas)):
+        carpetas[i] = carpetas[i][2:]
     return render(request, "index.html", {"ubicacion":ubicacion, "carpetas":carpetas})
